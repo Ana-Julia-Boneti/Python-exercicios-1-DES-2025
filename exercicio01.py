bedf80173df5a43c460faf3c3dce807ae34946ab
Ana-Julia-Boneti <ana.volgue@escola.pr.gov.br>
2025-06-02 14:11:40 +0000

    terminei 01 e 02 falta o 03

--- exercicio01.py
+++ exercicio01.py
@@ -3,5 +3,10 @@
 
 # Crie um programa que receba o número de avaliações de cada curso e exiba qual teve mais.
 # Caso as quantidades sejam iguais, exiba uma mensagem dizendo que houve empate.
+curso01 = int(input(" digite qual foi a avaliação do curso01"))
+curso02 = int(input("digite qual foi a avaliação do curso02"))
 
-gf
+if curso01>curso02:
+    print("curso01 teve mais avaliações")
+else:
+    print("curso02 teve menos avaliações")
--- exercicio02.py
+++ exercicio02.py
@@ -3,4 +3,12 @@
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
 
-bgf
+projeto_01 = int(input("primeiro projeto"))
+projeto_02 = int(input("segundo projeto"))
+projeto_03 = int(input("terceiro projeto"))
+
+if projeto_01 < 0 or projeto_02 < 0 or projeto_03 < 0:
+    print("erro, numero negativo")
+else:
+    soma = projeto_01 + projeto_02 + projeto_03
+    print("dias totais de projeto: {soma} dias")
