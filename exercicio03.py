3b890944598ca3befa5fe0a0ef5cfff9b04375ce
Ana-Julia-Boneti <ana.volgue@escola.pr.gov.br>
2025-06-03 12:18:57 +0000

    falta alguns

--- exercicio03.py
+++ exercicio03.py
@@ -1,3 +1,8 @@
 # Rafael trabalha com armazenamento de grãos e precisa garantir que a umidade do ar no local não ultrapasse 70%.
 # Escreva um programa que receba o valor da umidade atual e exiba um alerta se estiver acima do limite.
-umidade int(input(""))
+umidade_local =  int(input("digite o valor da umidade"))
+
+if umidade_local < 70:
+    print ("a umidade está correta")
+else:
+    print ("a umidade está muito baixa")
--- exercicio05.py
+++ exercicio05.py
@@ -1,3 +1,8 @@
 # Diego está acompanhando seu consumo de internet mensal, que tem um limite de 100 GB.
 # O programa deve receber o total consumido e avisar se ele ultrapassou o limite.
 
+int(input("digite o valor de internet consumidos no mes"))
+if < 100
+    print ("você ultrapassou o limite de internet")
+else
+    print ("você está dentro do limite de GB")
