de7011f979bda0f54b3f89d08f848b504e33a919
Ana-Julia-Boneti <ana.volgue@escola.pr.gov.br>
2025-06-16 14:06:02 +0000

    14 em andamento

--- exercicio14.py
+++ exercicio14.py
@@ -4,3 +4,10 @@
 #Acima de R$ 300,00 têm 5%
 #Menor ou igual a R$ 300,00 não têm desconto
 
+valor = int(input("digite o valor  compra"))
+if  valor <= 500:
+    desconto = ("voce ganhou 10% de desconto")
+elif valor >= 300:
+    desconto =  ("voce ganhou 5% de desconto")
+else:
+    print (f"o valor de {desconto} é a % {valor:.2f}")
