50635843a93ee5acc72b09863ba470109bf827df
Ana-Julia-Boneti <ana.volgue@escola.pr.gov.br>
2025-06-17 14:11:43 +0000

    14 pronto

---
+++ carta.py
@@ -0,0 +1,6 @@
+destinatario = input("digite o nome do destinatario:")
+mensagem = input("digite sua mensagem:")
+remetente = input("digite seu nome:")
+
+print("\n--- Sua carta ---")
+print(f"Olá {destinatario},\n\n{mensagem}\n\nemail enviado por {remetente}\n no dia 17/06/2025, \n{remetente}")
